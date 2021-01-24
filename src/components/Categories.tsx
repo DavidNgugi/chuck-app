cadf14c19fbd6fb7c6d2047745401c648d0d4766
David Ngugi <ndavidngugi@gmail.com>
2021-01-24 22:11:10 +0300

    UI update on categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -7,22 +7,30 @@ export const Categories = () => {
 		<JokeContext.Consumer>
 			{ value => 
 				(
-					<div className="row">
+					<div className="row category-body">
                         <h3 className="category-header">Joke Categories</h3>
-                        <ul className="nav d-flex">
-                            {
-                                value.categories.map((category, index) => {
-                                    return <li className = "nav-item p-1" key={index}>
-                                        <Link
-                                            to={`/jokes/${category}`}
-                                            type="button"
-                                            className="btn btn-sm btn-outline-secondary"
-                                        >
-                                            {category}
-                                        </Link></li>
-                                })
+                        <div className= "col-md-6">
+                            { value.new_categories_loading 
+                                ?
+                                    <p className = "p-1 text-center">Loading...</p>
+                                :
+                                <ul className="nav d-flex">
+                                    {
+                                        
+                                        value.categories.map((category, index) => {
+                                            return <li className = "nav-item p-1" key={index}>
+                                                <Link
+                                                    to={`/jokes/${category}`}
+                                                    type="button"
+                                                    className="btn btn-sm btn-outline-secondary"
+                                                >
+                                                    {category}
+                                                </Link></li>
+                                        })
+                                    }
+                                </ul>
                             }
-                        </ul>
+                        </div>
 					</div>
 				)
 			}
